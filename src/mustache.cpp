bfc4264c72cabeec900760b4b3adbd178f255910
Elvis Stansvik <elvstone@gmail.com>
2014-08-18 18:03:33 +0200

    Don't let delimiters set in partial affect parent.
    This fixes the "Post-Partial Behavior" test in delimiters.json.

--- src/mustache.cpp
+++ src/mustache.cpp
@@ -310,6 +310,9 @@ QString Renderer::render(const QString& _template, int startPos, int endPos, Con
 			break;
 		case Tag::Partial:
 		{
+			QString tagStartMarker = m_tagStartMarker;
+			QString tagEndMarker = m_tagEndMarker;
+
 			m_partialStack.push(tag.key);
 
 			QString partial = context->partialValue(tag.key);
@@ -317,6 +320,9 @@ QString Renderer::render(const QString& _template, int startPos, int endPos, Con
 			lastTagEnd = tag.end;
 
 			m_partialStack.pop();
+
+			m_tagStartMarker = tagStartMarker;
+			m_tagEndMarker = tagEndMarker;
 		}
 		break;
 		case Tag::SetDelimiter:
